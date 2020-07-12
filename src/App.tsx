af3f11fc7471b1329aff4cde0d1376f076bf2001
Otis Chiemezie <otisidevelop@gmail.com>
2020-07-13 00:13:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
 import NotFound from "./pages/404";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
     document.body.className ="app"
@@ -12,6 +13,7 @@ function App() {
         <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/create-account" component={CreateAccount} />
+            <Route path="/forgot-password" component={ForgotPassword} />
             <Route component={NotFound} />
         </Switch>
     );
