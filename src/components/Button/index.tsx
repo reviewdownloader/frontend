2c16150ee7b10e5fa204bb52156a6f2068e41c2b
Otis Chiemezie <otisidevelop@gmail.com>
2020-07-14 07:59:10 +0100

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,13 +2,13 @@ import React, { FC } from "react";
 
 import "./button.css";
 
-
 interface iProp {
     loading: boolean;
     type: ButtonType;
     children?: any;
     onClick?: any;
     className: String;
+    disabled?: boolean;
 }
 
 export enum ButtonType {
@@ -16,9 +16,9 @@ export enum ButtonType {
     button = "button",
 }
 
-const PrimaryButton: FC<iProp> = ({ loading, type, children, onClick, className }) => {
+const PrimaryButton: FC<iProp> = ({ loading, type, children, onClick, className, disabled = false }) => {
     return (
-        <button onClick={onClick} type={type} className={`${className}`}>
+        <button onClick={onClick} disabled={disabled} type={type} className={`${className}`}>
             {loading ? (
                 <div className="lds-ring">
                     <div></div>
