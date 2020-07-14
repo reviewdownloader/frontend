070801741cb34cbec53167fdb8b7aec33ac2cf0f
Otis Chiemezie <otisidevelop@gmail.com>
2020-07-14 07:59:36 +0100

    Create TopNavigation.tsx

---
+++ src/components/TopNavigation.tsx
@@ -0,0 +1,186 @@
+import React from "react";
+import { ChevronRight, Home, CreditCard, Users, User, Settings, ChevronDown, Activity, CheckCircle, ShoppingBag, Lock, ToggleRight, Database } from "@styled-icons/feather";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { authService } from "./../services/Authentication.Service";
+import LanguageChanger from "./LanguageChanger";
+import { Box } from "@styled-icons/feather/Box";
+
+const TopNavigation = () => {
+    const { t } = useTranslation();
+    const user = authService.GetUser();
+    return (
+        <>
+            <div className="border-b border-theme-24 -mt-10 md:-mt-5 -mx-3 sm:-mx-8 px-3 sm:px-8 pt-3 md:pt-0 mb-10">
+                <div className="top-bar-boxed flex items-center">
+                    <a href="/" className="-intro-x hidden md:flex">
+                        <img alt="Investment Bot" className="w-6" src="/dist/images/logo.svg" />
+                        <span className="text-white text-lg ml-3">
+                            Trade<span className="font-medium">Bot</span>
+                        </span>
+                    </a>
+
+                    <div className="-intro-x breadcrumb breadcrumb--light mr-auto">
+                        <a href="/app" className="">
+                            Application
+                        </a>
+                        <ChevronRight size={18} />
+                        <a href="/app" className="breadcrumb--active">
+                           {t("home.caption")}
+                        </a>
+                    </div>
+                    <div className="intro-x relative mr-3 sm:mr-6">
+                        <LanguageChanger />
+                    </div>
+                    <div className="intro-x dropdown w-8 h-8 relative">
+                        <div className="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in scale-110">
+                            <img alt={user.firstname} src={user.image || "/dist/images/profile-6.jpg"} />
+                        </div>
+                        <div className="dropdown-box mt-10 absolute w-56 top-0 right-0 z-20">
+                            <div className="dropdown-box__content box bg-theme-38 text-white">
+                                <div className="p-4 border-b border-theme-40">
+                                    <div className="font-medium">
+                                        {user.firstname} {user.lastname}
+                                    </div>
+                                    <div className="text-xs text-theme-41">Investor</div>
+                                </div>
+                                <div className="p-2">
+                                    <NavLink to="/app/profile" className="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md">
+                                        <User className="w-4 h-4 mr-2" size={18} /> Profile
+                                    </NavLink>
+
+                                    <NavLink to="/" className="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md">
+                                        <Lock className="w-4 h-4 mr-2" size={18} /> Reset Password
+                                    </NavLink>
+                                </div>
+                                <div className="p-2 border-t border-theme-40">
+                                    <NavLink to="/" className="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-theme-1 rounded-md">
+                                        <ToggleRight className="w-4 h-4 mr-2" size={18} /> Logout
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav className="top-nav">
+                <ul>
+                    <li>
+                        <NavLink exact to="/app" className="top-menu" activeClassName="top-menu--active">
+                            <div className="top-menu__icon">
+                                <Home size={18} />
+                            </div>
+                            <div className="top-menu__title"> {t("home.caption")} </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/app/user-investment" className="top-menu" activeClassName="top-menu--active">
+                            <div className="top-menu__icon">
+                                <CreditCard size={18} />
+                            </div>
+                            <div className="top-menu__title"> {t("investment")} </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/app/referral" className="top-menu" activeClassName="top-menu--active">
+                            <div className="top-menu__icon">
+                                <Users size={18} />
+                            </div>
+                            <div className="top-menu__title"> {t("referrer")} </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/app/profile" className="top-menu" activeClassName="top-menu--active">
+                            <div className="top-menu__icon">
+                                <User size={18} />
+                            </div>
+                            <div className="top-menu__title"> {t("profile")} </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact to="/app/setting" className="top-menu" activeClassName="top-menu--active">
+                            <div className="top-menu__icon">
+                                <Settings size={18} />
+                            </div>
+                            <div className="top-menu__title"> {t("setting")} </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <a href="javascript:;" className="top-menu">
+                            <div className="top-menu__icon">
+                                <Database size={18} />
+                            </div>
+                            <div className="top-menu__title">
+                                Management <ChevronDown size={18} />
+                            </div>
+                        </a>
+                        <ul className="">
+                            <li>
+                                <NavLink to="/app/plan" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <Activity size={18} />
+                                    </div>
+                                    <div className="top-menu__title">Plan Management </div>
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to="/app/load-investment" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <CreditCard size={18} />
+                                    </div>
+                                    <div className="top-menu__title">Load Investment </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/app/load-investment" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <Users size={18} />
+                                    </div>
+                                    <div className="top-menu__title">User Management</div>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:;" className="top-menu">
+                            <div className="top-menu__icon">
+                                <Box size={18}/>
+                            </div>
+                            <div className="top-menu__title">
+                                Admin Corner <ChevronDown size={18} />
+                            </div>
+                        </a>
+                        <ul className="">
+                            <li>
+                                <NavLink exact to="/app/investment-approval" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <CheckCircle size={18} />
+                                    </div>
+                                    <div className="top-menu__title"> Investment Approval </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink exact to="/app/active-investment" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <ShoppingBag size={18} />
+                                    </div>
+                                    <div className="top-menu__title"> Active Investment </div>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink exact to="/app/active-investment" className="top-menu">
+                                    <div className="top-menu__icon">
+                                        <CreditCard size={18} />
+                                    </div>
+                                    <div className="top-menu__title"> Referral Bonus</div>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+export default TopNavigation;
