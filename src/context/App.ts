4988e4159c25a948e71bc0670381b2db763478f7
Otis Chiemezie <otisidevelop@gmail.com>
2020-07-14 07:59:41 +0100

    Update App.ts

--- src/context/App.ts
+++ src/context/App.ts
@@ -11,3 +11,24 @@ export const AppUploadUrl = () => REACT_APP_UploadBaseUrl || "";
 export const UploadPreset = () => REACT_APP_UploadPreset || "";
 
 export const AppName = "New Investment Bot";
+
+export const CleanMessage = (message: string) => message.replace("GraphQL error:", "").replace("Network error:", "");
+
+export const CleanDate = (date: string, onlyDate = false, short = true) =>
+    onlyDate
+        ? Intl.DateTimeFormat("en-GB", {
+              month: short ? "short" : "long",
+              year: "numeric",
+              weekday: short ? "short" : "long",
+              day: "numeric",
+          }).format(new Date(date))
+        : Intl.DateTimeFormat("en-GB", {
+              hour: "numeric",
+              minute: "numeric",
+              second: "numeric",
+              hour12: false,
+              month: short ? "short" : "long",
+              year: "numeric",
+              weekday: short ? "short" : "long",
+              day: "numeric",
+          }).format(new Date(date));
